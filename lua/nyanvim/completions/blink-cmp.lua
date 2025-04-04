4b61881ac6db1d74e04241cfb7ad4836f859832d
Dileep Kishore <k.dileep1994@gmail.com>
2025-04-04 19:19:22 -0400

    feat(completion): fix blink.cmp & add config

--- lua/nyanvim/completions/blink-cmp.lua
+++ lua/nyanvim/completions/blink-cmp.lua
@@ -2,9 +2,74 @@ require('lze').load {
   {
     'blink.cmp',
     event = { 'DeferredUIEnter' },
-    opts = {
-      keymap = { preset = 'enter' },
-    },
+    on_require = 'blink',
+    after = function(_)
+      require('blink.cmp').setup {
+        appearance = { nerd_font_variant = 'normal' },
+        keymap = {
+          preset = 'enter',
+          ['<Tab>'] = {},
+          ['<S-Tab>'] = {},
+          ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
+          ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
+          ['<C-k>'] = { 'snippet_forward', 'fallback_to_mappings' },
+          ['<C-S-k>'] = { 'snippet_forward', 'fallback_to_mappings' },
+        },
+        signature = { enabled = true },
+        cmdline = {
+          enabled = true,
+          keymap = { preset = 'inherit' },
+          completion = {
+            list = { selection = { preselect = false } },
+            menu = { auto_show = true },
+            ghost_text = { enabled = true },
+          },
+        },
+        completion = {
+          list = { selection = { preselect = false } },
+          menu = {
+            border = {
+              { '󱐋', 'WarningMsg' },
+              '─',
+              '╮',
+              '│',
+              '╯',
+              '─',
+              '╰',
+              '│',
+            },
+            winhighlight = 'Normal:Pmenu,CursorLine:PmenuSel,Search:None',
+          },
+          documentation = {
+            auto_show = true,
+            window = {
+              border = {
+                { '󰙎', 'WarningMsg' },
+                '─',
+                '╮',
+                '│',
+                '╯',
+                '─',
+                '╰',
+                '│',
+              },
+              winhighlight = 'Normal:Pmenu,CursorLine:PmenuSel,Search:None',
+            },
+          },
+          ghost_text = { enabled = true },
+        },
+        sources = {
+          default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
+          providers = {
+            lazydev = {
+              name = 'LazyDev',
+              module = 'lazydev.integrations.blink',
+              score_offset = 100,
+            },
+          },
+        },
+      }
+    end,
   },
   {
     'friendly-snippets',
--- lua/nyanvim/lsps/caps-on_attach.lua
+++ lua/nyanvim/lsps/caps-on_attach.lua
@@ -7,13 +7,13 @@ function M.on_attach(_, bufnr)
     vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
   end
 
-  -- NOTE: I am going to use LSPsaga for these keymaps
-  -- nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  -- nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-  -- nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-  -- nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-  -- nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-  -- nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+  -- TODO: Remove these after adding lsp_saga
+  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+  nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+  nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+  nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
   nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
   -- Create a command `:Format` local to the LSP buffer
@@ -24,12 +24,7 @@ end
 
 function M.get_capabilities(server_name)
   local capabilities = vim.lsp.protocol.make_client_capabilities()
-  capabilities = vim.tbl_deep_extend(
-    'force',
-    capabilities,
-    require('blink.cmp').get_lsp_capabilities({}, false)
-  )
-  capabilities.textDocument.completion.completionItem.snippetSupport = true
+  capabilities = require('blink.cmp').get_lsp_capabilities(capabilities)
   return capabilities
 end
 return M
