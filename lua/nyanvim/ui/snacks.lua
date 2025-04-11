7272998cef694552930f042ae6fbdc56cf09d448
Dileep Kishore <k.dileep1994@gmail.com>
2025-04-11 01:21:22 -0400

    feat(ui): add snacks pickers + other module config

--- lua/nyanvim/ui/snacks.lua
+++ lua/nyanvim/ui/snacks.lua
@@ -6,11 +6,68 @@ require('lze').load {
       local snacks_dashboard = require 'nyanvim.ui.snacks-dashboard'
 
       snacks.setup {
+        styles = {
+          lazygit = {
+            border = 'rounded',
+            backdrop = false,
+          },
+        },
+        bigfile = { enabled = true },
+        gitbrowse = { enabled = true },
+        lazygit = {
+          enabled = true,
+          win = { position = 'float' },
+        },
+        quickfile = { enabled = true },
         dashboard = snacks_dashboard.dashboard,
         notifier = {
           enabled = true,
           style = 'compact',
           timeout = 2500,
+        },
+        picker = {
+          layouts = {
+            custom = {
+              preset = 'default',
+              layout = { backdrop = false },
+            },
+            vscode_custom = {
+              preset = 'vscode',
+              layout = { border = 'rounded' },
+            },
+          },
+          sources = {
+            -- Snacks pickers (find) [f]
+            files = { layout = { preset = 'custom' } },
+            git_files = { layout = { preset = 'custom' } },
+            buffers = { layout = { preset = 'dropdown' } },
+            projects = { layout = { preset = 'vscode_custom' } },
+            jumps = { layout = { preset = 'ivy' } },
+            marks = { layout = { preset = 'ivy' } },
+
+            -- Snacks pickers (git) [g]
+            git_log = { layout = { preset = 'vertical' } },
+            git_log_file = { layout = { preset = 'vertical' } },
+
+            -- Snacks pickers (search) [s]
+            grep = { layout = { preset = 'ivy' } },
+            -- lines = {layout = {preset = "ivy"}},
+            grep_buffers = { layout = { preset = 'ivy' } },
+            todo_comments = { layout = { preset = 'ivy' } },
+
+            -- Snacks pickers (misc) [t]
+            command_history = { layout = { preset = 'vscode_custom' } },
+            registers = { layout = { preset = 'vscode_custom' } },
+            undo = { layout = { preset = 'dropdown' } },
+            help = { layout = { preset = 'custom' } },
+            highlights = { layout = { preset = 'dropdown' } },
+            icons = { layout = { preset = 'vscode_custom' } },
+            keymaps = { layout = { preset = 'vscode_custom' } },
+
+            -- Snacks pickers (lsp) [l]
+            diagnostics = { layout = { preset = 'ivy' } },
+            lsp_symbols = { layout = { preset = 'custom' } },
+          },
         },
       }
     end,
