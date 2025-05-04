2c3a0055ebd12d249528196c43c721d88b29ac74
Dileep Kishore <k.dileep1994@gmail.com>
2025-05-04 15:11:06 -0400

    feat(ui): faster smear effect

--- lua/nyanvim/ui/smear.lua
+++ lua/nyanvim/ui/smear.lua
@@ -4,12 +4,11 @@ require('lze').load {
     event = 'DeferredUIEnter',
     after = function(_)
       require('smear_cursor').setup {
-        -- NOTE: Uncomment to enable the faster smear effect
-        -- stiffness = 0.8,
-        -- trailing_stiffness = 0.5,
-        -- stiffness_insert_mode = 0.6,
-        -- trailing_stiffness_insert_mode = 0.6,
-        -- distance_stop_animating = 0.5,
+        stiffness = 0.8,
+        trailing_stiffness = 0.5,
+        stiffness_insert_mode = 0.6,
+        trailing_stiffness_insert_mode = 0.6,
+        distance_stop_animating = 0.5,
       }
     end,
   },
